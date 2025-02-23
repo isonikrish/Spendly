83a716389785dd5be337d661ceb10ad1da8b3179
Jagadeesh <jagadeesh28.m@gmail.com>
2025-02-23 15:38:51 +0530

    removed conflicts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,13 @@
-<<<<<<< HEAD
 import DashboardHeroSection from "@/components/DashboardHeroSection";
 import DashboardMainContent from "@/components/DashboardMainContent";
-=======
-"use client"
-import useApp from "@/stores/useApp"
->>>>>>> 52528b85859e6443957a1724c9c7f9e0506ae870
 
 function Dashboard() {
-  const { user } = useApp();
   return (
-<<<<<<< HEAD
     <div className="p-10 h-auto w-full">
       <DashboardHeroSection />
       <DashboardMainContent />
     </div>
   );
-=======
-    <div className="bg- p-8">
-      <h2 className="text-4xl font-bold">Hi, {user?.name} 👋</h2>
-      <p className="text-gray-500 mt-2">
-        Here's what happenning with your money, Lets Manage your expense
-      </p>
-    </div>
-  )
->>>>>>> 52528b85859e6443957a1724c9c7f9e0506ae870
 }
 
 export default Dashboard;
